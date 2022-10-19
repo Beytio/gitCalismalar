0285376e3c7f3134e4402565a450a7b30ce7f031
beytio <oguzbeytullah@gmail.com>
2022-10-19 21:10:18 +0300

    added new information

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -6,10 +6,16 @@ public class gitClass {
         System.out.println("ikinci satırımda çeşitli kodlar yazıyorum");
         String name = "Hasan";
         String surname="Kocak";
+
+
         int studentId = 125;
+        String namme2="klkdsj";
+
         String adress="Eryaman Mahallesi Başaka siter";
         String town="Kırıkkale";
         String country="Turkiye";
+
+
 
 
 
