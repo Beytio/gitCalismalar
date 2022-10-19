2828af697ab0665f727fdade164d47b23a81d75e
beytio <oguzbeytullah@gmail.com>
2022-10-19 19:54:09 +0300

    Added new methods

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -3,5 +3,12 @@ package gitPackage;
 public class gitClass {
     public static void main(String[] args) {
         System.out.println("git çalışmaları kapasamında oluşturuldu");
+
+        System.out.println("ikinci satırımda çeşitli kodlar yazıyorum");
+
+        String str = "Hasan";
+        String surname="Kocak";
+        int studentId = 125;
+
     }
 }
