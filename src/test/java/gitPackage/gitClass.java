fa02d187b379842941d8cf9b467cc4786c646085
beytio <oguzbeytullah@gmail.com>
2022-10-19 20:27:29 +0300

    Change everything

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -2,19 +2,6 @@ package gitPackage;
 
 public class gitClass {
     public static void main(String[] args) {
-        System.out.println("git çalışmaları kapasamında oluşturuldu");
-
-        System.out.println("ikinci satırımda çeşitli kodlar yazıyorum");
-
-        String name = "Hasan";
-        String surname="Kocak";
-        int studentId = 125;
-
-        String adress="Eryaman Mahallesi Başaka siter";
-        String town="Ankara";
-        String country="Turkiye";
-
-
 
     }
 }
